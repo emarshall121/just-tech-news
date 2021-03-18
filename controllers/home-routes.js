c7da4738af715aa9d571d2cf19667d175ba967a3
EMarshall <emilyjmarshall121@gmail.com>
2021-03-17 17:56:20 -0700

    logout functionality:

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,7 +37,17 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
 router.get('/', (req, res) => {
+  console.log(req.session);
   res.render('homepage');
 });
 
--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 // Set up handlebars as template engine
 const exphbs = require('express-handlebars');
